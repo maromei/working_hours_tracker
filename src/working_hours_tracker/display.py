f7cd7d79253194728068e202ec1039872d94ac88
maromei <maromei@proton.me>
2024-04-05 14:17:43 +0000

    WIP switching to 1 button

--- src/working_hours_tracker/display.py
+++ src/working_hours_tracker/display.py
@@ -11,6 +11,8 @@ class MainWindow(QMainWindow):
     start_time_btn: QPushButton
     end_time_btn: QPushButton
 
+    start_end_time_btn: QPushButton
+
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
@@ -20,6 +22,20 @@ class MainWindow(QMainWindow):
 
         self.start_time_btn.pressed.connect(self.write_start_time)
         self.end_time_btn.pressed.connect(self.write_end_time)
+
+        self.start_end_time_btn.pressed.connect(self.toggle_write_time)
+
+    def toggle_write_time(self):
+
+        now = datetime.now()
+        time_file_path = settings.get_time_file_path()
+
+        if time_file.has_open_time(time_file_path):
+            time_file.add_end_time(time_file_path, now)
+            self.start_end_time_btn.setText("Start")
+        else:
+            time_file.add_start_time(time_file_path, now)
+            self.start_end_time_btn.setText("End")
 
     def write_start_time(self):
 
